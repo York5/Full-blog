86d9ccf9dae1778449ccb3562fef0c9678262c23
York5 <mikhailvishin2@yandex.com>
2019-10-01 15:30:18 +0600

    Blog cloned from repo, templates rewritten to include bootstrap design. Article CRUD views rewritten to include generic View and Template View classes

---
+++ source/webapp/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False, verbose_name='Title')
+    text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Text')
+    author = models.CharField(max_length=40, null=False, blank=False, default='Unknown', verbose_name='Author')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created At')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated At')
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, blank=True, verbose_name='Category',
+                                 related_name='articles')
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    article = models.ForeignKey('webapp.Article', related_name='comments',
+                                on_delete=models.CASCADE, verbose_name='Article')
+    text = models.TextField(max_length=400, verbose_name='Comment')
+    author = models.CharField(max_length=40, null=True, blank=True, default='Anonymous', verbose_name='Author')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created At')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated At')
+
+    def __str__(self):
+        return self.text[:20]
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20, verbose_name='Name')
+
+    def __str__(self):
+        return self.name
---
+++ source/webapp/views.py
@@ -0,0 +1,84 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from webapp.forms import ArticleForm
+from webapp.models import Article
+from django.views import View
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['articles'] = Article.objects.all()
+        return context
+
+
+class ArticleView(TemplateView):
+    template_name = 'article.html'
+
+    def get_context_data(self, **kwargs):
+        pk = kwargs.get('pk')
+        context = super().get_context_data(**kwargs)
+        context['article'] = get_object_or_404(Article, pk=pk)
+        return context
+
+
+class ArticleCreateView(View):
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, 'create.html', context={'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(data=request.POST)
+        if form.is_valid():
+            article = Article.objects.create(
+                title=form.cleaned_data['title'],
+                author=form.cleaned_data['author'],
+                text=form.cleaned_data['text'],
+                category=form.cleaned_data['category']
+            )
+            return redirect('article_view', pk=article.pk)
+        else:
+            return render(request, 'create.html', context={'form': form})
+
+
+class ArticleUpdateView(View):
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        article = get_object_or_404(Article, pk=pk)
+        form = ArticleForm(data={
+            'title': article.title,
+            'author': article.author,
+            'text': article.text,
+            'category': article.category_id
+        })
+        return render(request, 'update.html', context={'form': form, 'article': article})
+
+    def post(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        article = get_object_or_404(Article, pk=pk)
+        form = ArticleForm(data=request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            article.title = data['title']
+            article.author = data['author']
+            article.text = data['text']
+            article.category = data['category']
+            article.save()
+            return redirect('article_view', pk=article.pk)
+        else:
+            return render(request, 'update.html', context={'form': form, 'article': article})
+
+
+class ArticleDeleteView(View):
+    def get(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        article = get_object_or_404(Article, pk=pk)
+        return render(request, 'delete.html', context={'article': article})
+
+    def post(self, request, *args, **kwargs):
+        pk = kwargs.get('pk')
+        article = get_object_or_404(Article, pk=pk)
+        article.delete()
+        return redirect('index')
